60be0972c9a6fa1ed049c62cee3ab1570f8e55a3
pmackovski <phil.mackovski@servicemesh.com>
2016-08-25 16:04:21 +1000

    added controlEC2Instance.sh

---
+++ controlEC2Instance.sh
@@ -0,0 +1,26 @@
+#controlEC2Instance.sh
+#Phil Mackovski
+
+#stop/start EC2 instances based on "owner" tag
+
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <stop|start>"
+    exit 1
+fi
+
+OWNER=Phil
+
+#retrieve instances based on owner tag
+INSTANCES=$(aws ec2 describe-tags --filters "Name=key,Values=Owner" "Name=value,Values=$OWNER" --output text | awk '{print $3}')
+
+#stop or start instances
+case "$1" in
+    stop)
+        aws ec2 stop-instances --instance-ids $INSTANCES
+        ;;
+    start)
+        aws ec2 start-instances --instance-ids $INSTANCES
+        ;;
+esac
