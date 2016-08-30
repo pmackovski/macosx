c12455a4def5d6d46dff6fc86ea4c016b61e84e1
Phil Mackovski <pmackovski@csc.com>
2016-08-30 02:50:42 +0000

    add filter for instances only

--- controlEC2Instance.sh
+++ controlEC2Instance.sh
@@ -13,7 +13,9 @@ fi
 OWNER=Phil
 
 #retrieve instances based on "Owner" tag
-INSTANCES=$(aws ec2 describe-tags --filters "Name=key,Values=Owner" "Name=value,Values=$OWNER" --output text | awk '{print $3}')
+INSTANCES=$(aws ec2 describe-tags --filters "Name=key,Values=Owner" "Name=value,Values=$OWNER" --output text | grep "i-" | awk '{print $3}')
+
+echo "Instances: $INSTANCES"
 
 #stop or start instances
 case "$1" in
